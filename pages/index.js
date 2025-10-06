f71dbcff6f1e29e67ae240a4d1d9db7ccff6ee6b
Imran13k <imranhossain13k@gmail.com>
2025-10-06 20:23:38 +0600

    Create index.js

---
+++ pages/index.js
@@ -0,0 +1,16 @@
+export default function Home() {
+  return (
+    <div style={{
+      textAlign: 'center',
+      marginTop: '100px',
+      fontFamily: 'Arial, sans-serif',
+      backgroundColor: '#f5f5f5',
+      height: '100vh',
+      paddingTop: '80px'
+    }}>
+      <h1>🏆 Welcome to SportzNow!</h1>
+      <p>Live Sports Score Website Coming Soon...</p>
+      <p>Stay tuned for Cricket 🏏, Football ⚽, and more!</p>
+    </div>
+  );
+}
